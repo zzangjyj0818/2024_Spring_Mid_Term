09504a25101f51b27d710cd4d8db32fe5a0b45f4
zzangjyj0818 <jyj010818@naver.com>
2024-10-25 01:54:39 +0900

    fix

--- src/main/java/webserver/mid/global/config/WebConfig.java
+++ src/main/java/webserver/mid/global/config/WebConfig.java
@@ -2,8 +2,10 @@ package webserver.mid.global.config;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import webserver.mid.global.filter.LoginFilter;
 
+@Configuration
 public class WebConfig {
     @Bean
     public FilterRegistrationBean logFilter() {
